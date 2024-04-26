9135fac3cddfa37e9b744c512e98954489b99988
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2024-04-26 14:31:34 +1000

    test

--- configure_demo/vault.tf
+++ configure_demo/vault.tf
@@ -46,8 +46,8 @@ resource "vault_jwt_auth_backend_role" "example" {
   token_policies    = [vault_policy.tfc.name]
   token_max_ttl     = "3600"
   bound_audiences   = ["https://github.com/${var.github_organization}"]
-  bound_claims_type = "glob"
-  bound_claims     = { repo = "${var.github_organization}/*" }
+  bound_claims_type = "string"
+  bound_subject     = "repo:${var.github_organization}/${var.github_repository}:pull_request"
   user_claim        = "actor"
   role_type         = "jwt"
 }
